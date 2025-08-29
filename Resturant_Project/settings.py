5f9c5f22d80a6053813869059134b6507a3634f5
Sharjeel <sa062385@gmail.com>
2025-08-29 22:30:56 +0500

    Fresh

--- Resturant_Project/settings.py
+++ Resturant_Project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +64,11 @@ WSGI_APPLICATION = 'Resturant_Project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_check=True,
+    )
 }
 
 # Password validation
@@ -85,7 +87,7 @@ USE_TZ = True
 
 # ✅ Static & Media
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
