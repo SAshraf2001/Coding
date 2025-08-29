91449f893d6ecd777b289c176c7243f753a49d5e
Sharjeel <sa062385@gmail.com>
2025-08-29 22:39:09 +0500

    Changed

--- Resturant_Project/settings.py
+++ Resturant_Project/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,11 +63,10 @@ WSGI_APPLICATION = 'Resturant_Project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        conn_health_check=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
